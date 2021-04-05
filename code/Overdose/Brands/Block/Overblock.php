a9125be75b6857631a51db43a25059d5f78c6f6f
Dmytro <daymonmoss@gmail.com>
2021-04-05 12:50:22 +0300

    Deploy All

---
+++ code/Overdose/Brands/Block/Overblock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Overdose\Brands\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Overblock extends Template
+{
+    public function showBlock()
+    {
+        return "It is Overdose Brands Block!";
+    }
+}
