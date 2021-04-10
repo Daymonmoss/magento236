35848fd195a683667784e746c422542eaf59ecf9
Dmytro <daymonmoss@gmail.com>
2021-04-10 23:27:51 +0300

    Deploy All

--- code/Overdose/Brands/Plugin/AfterPlugin.php
+++ code/Overdose/Brands/Plugin/AfterPlugin.php
@@ -2,10 +2,11 @@
 namespace Overdose\Brands\Plugin;
 
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Api\Data\ProductSearchResultsInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Overdose\Brands\Api\BrandsRepositoryInterface;
+
 class AfterPlugin
 {
 
@@ -27,38 +28,44 @@ class AfterPlugin
         $this->brandsRepository = $brandsRepository;
     }
 
-    public function afterGet(productRepositoryInterface $subject, ProductInterface $brands)
+    public function afterGet(productRepositoryInterface $subject, ProductInterface $entity)
     {
         $collection = $this->collectionFactory->create();
 
-        foreach ($subject as $brand) {
-            $count = $collection->getAttributeValueCount('overdose_brand');
-            $extensionAttributes = $brand->getExtensionAttributes();
-            $extensionAttributes->setTotalBrandProducts($count);
-        }
-        return $brands;
+        $count = $collection->getAttributeValueCount('overdose_brand');
+
+        $extensionAttributes = $entity->getExtensionAttributes();
+        $extensionAttributes->setTotalBrandProducts($count);
+        $entity->setExtensionAttributes($extensionAttributes);
+
+        return $entity;
     }
 
-    public function afterGeById(ProductRepositoryInterface $subject, ProductInterface $product)
+    public function afterGeById(ProductRepositoryInterface $subject, ProductInterface $entity)
     {
         $collection = $this->collectionFactory->create();
 
-        foreach ($subject as $brand) {
-            $count = $collection->getAttributeValueCount('overdose_brand');
-            $extensionAttributes = $brand->getExtensionAttributes();
-            $extensionAttributes->setTotalBrandProducts($count);
-        }
-        return $product;
+        $count = $collection->getAttributeValueCount('overdose_brand');
+
+        $extensionAttributes = $entity->getExtensionAttributes();
+        $extensionAttributes->setTotalBrandProducts($count);
+        $entity->setExtensionAttributes($extensionAttributes);
+
+        return $entity;
     }
 
     public function afterGetList(ProductRepositoryInterface $subject, ProductSearchResultsInterface $searchCriteria):ProductSearchResultsInterface
     {
         $collection = $this->collectionFactory->create();
         $products = [];
-        foreach ($searchCriteria->getItems() as $brand) {
+        foreach ($searchCriteria->getItems() as $entity) {
             $count = $collection->getAttributeValueCount('overdose_brand');
-            $extensionAttributes = $brand->getExtensionAttributes();
+
+            $extensionAttributes = $entity->getExtensionAttributes();
             $extensionAttributes->setTotalBrandProducts($count);
+            $entity->setExtensionAttributes($extensionAttributes);
+
+            $products[] = $entity;
         }
         $searchCriteria->setItems($products);
         return $searchCriteria;
